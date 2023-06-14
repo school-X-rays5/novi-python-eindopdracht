d9e11e8a7ee9974cbe62157cd9daf183bebee892
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2023-06-14 12:08:55 +0200

    added company data editing

--- src/company.py
+++ src/company.py
@@ -1,8 +1,11 @@
+import os
+
 import coordinate
 import gasses
 import globals as G
 
 FINE_AMOUNT = 1000
+
 
 def calculate_total_emissions(x, y):
     class_1_emissions = 0  # 100%
@@ -31,8 +34,15 @@ def calculate_total_emissions(x, y):
     total_emissions = class_1_emissions + class_2_emissions + class_3_emissions
     return total_emissions
 
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 class Company:
-    def __init__(self, code: str, name: str, street: str, house_number: str, postal_code: str, city: str, x: str, y: str, max_emissions: str, emissions: str, fine: str, check: str, check_freq: str, contact_person: str):
+    def __init__(self, code: str, name: str, street: str, house_number: str, postal_code: str, city: str, x: str,
+                 y: str, max_emissions: str, emissions: str, fine: str, check: str, check_freq: str,
+                 contact_person: str):
         self.__code = int(code)
         self.__name = name
         self.__street = street
@@ -51,44 +61,289 @@ class Company:
     def get_code(self) -> int:
         return self.__code
 
+    def set_code(self, code: int):
+        self.__code = code
+
+    def set_code_input(self):
+        clear_terminal()
+
+        code = input("Enter code: ")
+        if not (len(code) <= 4):
+            print("Code must be 4 digits long")
+            self.set_code_input()
+            return
+
+        if not code.isdigit():
+            print("Code must be digits only")
+            self.set_code_input()
+            return
+
+        self.__code = int(code)
+
     def get_name(self) -> str:
         return self.__name
+
+    def set_name(self, name: str):
+        self.__name = name
+
+    def set_name_input(self):
+        clear_terminal()
+
+        name = input("Enter name: ")
+        if not (len(name) <= 20):
+            print("Name max length is 20 characters long")
+            self.set_name_input()
+            return
+
+        self.__name = name
 
     def get_street(self) -> str:
         return self.__street
 
+    def set_street(self, street: str):
+        self.__street = street
+
+    def set_street_input(self):
+        clear_terminal()
+
+        street = input("Enter street: ")
+        if not (len(street) <= 30):
+            print("Street max length is 30 characters long")
+            self.set_street_input()
+            return
+
+        self.__street = street
+
     def get_house_number(self) -> str:
         return self.__house_number
+
+    def set_house_number(self, house_number: str):
+        self.__house_number = house_number
+
+    def set_house_number_input(self):
+        clear_terminal()
+
+        house_number = input("Enter house number: ")
+        if not (len(house_number) <= 5):
+            print("House number max length is 5 characters long")
+            self.set_house_number_input()
+            return
+
+        self.__house_number = house_number
 
     def get_postal_code(self) -> str:
         return self.__postal_code
 
+    def set_postal_code(self, postal_code: str):
+        self.__postal_code = postal_code
+
+    def set_postal_code_input(self):
+        clear_terminal()
+
+        postal_code = input("Enter postal code: ")
+        if not (len(postal_code) == 6 and postal_code[:4].isdigit() and postal_code[4:].isalpha()):
+            print("Postal code must be in the format 'nnnncc'")
+            self.set_postal_code_input()
+            return
+
+        self.__postal_code = postal_code
+
     def get_city(self) -> str:
         return self.__city
+
+    def set_city(self, city: str):
+        self.__city = city
+
+    def set_city_input(self):
+        clear_terminal()
+
+        city = input("Enter city: ")
+        if not (len(city) <= 20):
+            print("City max length is 20 characters long")
+            self.set_city_input()
+            return
+
+        self.__city = city
 
     def get_x(self) -> int:
         return self.__x
 
+    def set_x(self, x: int):
+        self.__x = x
+
+    def set_x_input(self):
+        clear_terminal()
+
+        x = input("Enter x coordinate: ")
+        if not (len(x) == 2):
+            print("X coordinate must be 2 digits long")
+            self.set_x_input()
+            return
+
+        if not x.isdigit():
+            print("X coordinate must be digits only")
+            self.set_x_input()
+            return
+
+        self.__x = int(x)
+
     def get_y(self) -> int:
         return self.__y
+
+    def set_y(self, y: int):
+        self.__y = y
+
+    def set_y_input(self):
+        clear_terminal()
+
+        y = input("Enter y coordinate: ")
+        if not (len(y) == 2):
+            print("Y coordinate must be 2 digits long")
+            self.set_y_input()
+            return
+
+        if not y.isdigit():
+            print("Y coordinate must be digits only")
+            self.set_y_input()
+            return
+
+        self.__y = int(y)
 
     def get_max_emissions(self) -> int:
         return self.__max_emissions
 
+    def set_max_emissions(self, max_emissions: int):
+        self.__max_emissions = max_emissions
+
+    def set_max_emissions_input(self):
+        clear_terminal()
+
+        max_emissions = input("Enter max emissions: ")
+        if not (len(max_emissions) <= 10):
+            print("Max emissions max length is 10 digits")
+            self.set_max_emissions_input()
+            return
+
+        if not max_emissions.isdigit():
+            print("Max emissions must be digits only")
+            self.set_max_emissions_input()
+            return
+
+        self.__max_emissions = int(max_emissions)
+
     def get_emissions(self) -> int:
         return self.__emissions
+
+    def set_emissions(self, emissions: int):
+        self.__emissions = emissions
+
+    def set_emissions_input(self):
+        clear_terminal()
+
+        emissions = input("Enter emissions (leave empty for no value): ")
+        if emissions.strip():
+            if not (len(emissions) <= 10):
+                print("Emissions max length is 10 digits")
+                self.set_emissions_input()
+                return
+
+            if not emissions.isdigit():
+                print("Emissions must be digits only")
+                self.set_emissions_input()
+                return
+
+            self.__emissions = int(emissions)
+        else:
+            self.__emissions = None
 
     def get_fine(self) -> int:
         return self.__fine
 
+    def set_fine(self, fine: int):
+        self.__fine = fine
+
+    def set_fine_input(self):
+        clear_terminal()
+
+        fine = input("Enter fine (leave empty for no value): ")
+        if fine.strip():
+            if not (len(fine) <= 8):
+                print("Fine max length is 10 digits")
+                self.set_fine_input()
+                return
+
+            if not fine.isdigit():
+                print("Fine must be digits only")
+                self.set_fine_input()
+                return
+
+            self.__fine = int(fine)
+        else:
+            self.__fine = None
+
     def get_check(self) -> str:
         return self.__check
+
+    def set_check(self, check: str):
+        self.__check = check
+
+    def set_check_input(self):
+        clear_terminal()
+
+        check = input("Enter check: ")
+        check = check.lower()  # Convert input to uppercase for case-insensitive comparison
+
+        if check not in ["j", "j"]:
+            print("Check must be 'j' or 'n'")
+            self.set_check_input()
+            return
+
+        self.__check = check
 
     def get_check_freq(self) -> int:
         return self.__check_freq
 
+    def set_check_freq(self, check_freq: int):
+        self.__check_freq = check_freq
+
+    def set_check_freq_input(self):
+        clear_terminal()
+
+        check_freq = input("Enter check frequency (leave empty for no value): ")
+        if check_freq.strip():
+            if not (len(check_freq) <= 2):
+                print("Check frequency max length is 2 digits")
+                self.set_check_freq_input()
+                return
+
+            if not check_freq.isdigit():
+                print("Check frequency must be digits only")
+                self.set_check_freq_input()
+                return
+
+            self.__check_freq = int(check_freq)
+        else:
+            self.__check_freq = None
+
     def get_contact_person(self) -> str:
         return self.__contact_person
+
+    def set_contact_person(self, contact_person: str):
+        self.__contact_person = contact_person
+
+    def set_contact_person_input(self):
+        clear_terminal()
+
+        contact_person = input("Enter contact person (leave empty for no value): ")
+        if contact_person.strip():
+            if not (len(contact_person) <= 30):
+                print("Contact person max length is 30 characters long")
+                self.set_contact_person_input()
+                return
+
+            self.__contact_person = contact_person
+        else:
+            self.__contact_person = None
 
     def print_data(self) -> None:
         print(f"Company Code: {self.__code}", end=", ")
@@ -106,7 +361,7 @@ class Company:
         print(f"Check Frequency: {self.__check_freq}", end=", ")
         print(f"Contact Person: {self.__contact_person.strip()}")
 
-    def calculate_fine(self) -> int:
+    def set_fine_from_loaded_measurements(self) -> int:
         self.__emissions = calculate_total_emissions(self.__x, self.__y)
         emissions_left = self.__max_emissions - self.__emissions
         if not (emissions_left < 0):
@@ -114,6 +369,10 @@ class Company:
 
         self.__fine = round(abs(emissions_left) * FINE_AMOUNT)
         return self.__fine
+
+
+def create_empty_company() -> Company:
+    return Company("0", "", "", "", "", "", "0", "0", "0", "", "", "", "", "")
 
 
 def parse_companies(file_path) -> list[Company]:
@@ -124,7 +383,10 @@ def parse_companies(file_path) -> list[Company]:
     companies = []
     for line in lines:
         try:
-            companies.append(Company(line[0:4], line[4:24], line[24:54], line[54:59], line[59:65], line[65:85], line[85:87], line[87:89], line[89:99], line[99:109], line[109:117], line[117:118], line[118:120], line[120:140]))
+            companies.append(
+                Company(line[0:4], line[4:24], line[24:54], line[54:59], line[59:65], line[65:85], line[85:87],
+                        line[87:89], line[89:99], line[99:109], line[109:117], line[117:118], line[118:120],
+                        line[120:140]))
         except ValueError:
             print("Invalid company data:", line)
 
--- src/menu.py
+++ src/menu.py
@@ -9,8 +9,8 @@ import company as company_struct
 import gasses
 import globals as G
 import inspector as inspector_struct
-import report as report_struct
 import menu_controller
+import report as report_struct
 
 
 def clear_terminal():
@@ -127,6 +127,7 @@ def print_reports_per_company():
 
     pause_terminal()
 
+
 def load_measurement_file():
     path = input("Path to measurement file: ")
     if not os.path.isfile(path) or not path.endswith(".csv"):
@@ -154,40 +155,207 @@ def plot_weighted():
 
     for i in range(10000):
         weighted_gasses[i] = gasses.calculate_weighted_emissions(G.loaded_measurement[i][2],
-                                                                  G.loaded_measurement[i][3],
-                                                                  G.loaded_measurement[i][4],
-                                                                  G.loaded_measurement[i][5])
+                                                                 G.loaded_measurement[i][3],
+                                                                 G.loaded_measurement[i][4],
+                                                                 G.loaded_measurement[i][5])
 
     plt.imshow(weighted_gasses.reshape(100, 100))
     plt.colorbar()
     plt.show()
+
 
 def print_above_average_unknown_gas_concentration():
     for concentration in gasses.get_above_average_unknown_gas_concentrations(G.loaded_measurement):
         concentration.print_data()
     pause_terminal()
 
+
 def print_high_unknown_gas_concentration():
-    gasses.get_high_unknown_gas_concentration(G.loaded_measurement, gasses.get_company_areas()).print_data()
+    gasses.get_high_unknown_gas_concentration(G.loaded_measurement).print_data()
     pause_terminal()
+
 
 def show_company_fines():
     for company in G.companies:
-        fine = company.calculate_fine()
+        fine = company.set_fine_from_loaded_measurements()
         print("Fine for", company.get_name().strip(), "is", format(fine, ','), "euro")
     pause_terminal()
 
+
+def select_company() -> int:
+    clear_terminal()
+    for i, company in enumerate(G.companies):
+        print(i + 1, company.get_name().strip())
+
+    choice = input("Enter choice: ")
+    if choice.isdigit() and (0 > int(choice) and int(choice) <= len(G.companies)):
+        print("Invalid choice")
+        pause_terminal()
+        return select_company()
+
+    clear_terminal()
+    return int(choice) - 1
+
+
+def add_company():
+    company = company_struct.create_empty_company()
+    company.set_code_input()
+    company.set_name_input()
+    company.set_street_input()
+    company.set_house_number_input()
+    company.set_postal_code_input()
+    company.set_city_input()
+    company.set_x_input()
+    company.set_y_input()
+    company.set_max_emissions_input()
+    company.set_emissions_input()
+    company.set_fine_input()
+    company.set_check_input()
+    company.set_check_freq_input()
+    company.set_contact_person_input()
+    G.companies.append(company)
+    print("Company added")
+    company.print_data()
+    pause_terminal()
+
+
+def edit_company():
+    selected = select_company()
+    print("1. Code"
+          "\n2. Name"
+          "\n3. Street"
+          "\n4. House number"
+          "\n5. Postal code"
+          "\n6. City"
+          "\n7. X"
+          "\n8. Y"
+          "\n9. Max emissions"
+          "\n10. Emissions"
+          "\n11. Fine"
+          "\n12. Check"
+          "\n13. Check frequency"
+          "\n14. Contact person")
+
+    choice = input("Enter choice: ")
+    if not choice.isdigit() or not (0 < int(choice) <= 14):
+        print("Invalid choice")
+        pause_terminal()
+        edit_company()
+        return
+
+    company = G.companies[selected]
+
+    if choice == "1":
+        company.set_code_input()
+    elif choice == "2":
+        company.set_name_input()
+    elif choice == "3":
+        company.set_street_input()
+    elif choice == "4":
+        company.set_house_number_input()
+    elif choice == "5":
+        company.set_postal_code_input()
+    elif choice == "6":
+        company.set_city_input()
+    elif choice == "7":
+        company.set_x_input()
+    elif choice == "8":
+        company.set_y_input()
+    elif choice == "9":
+        company.set_max_emissions_input()
+    elif choice == "10":
+        company.set_emissions_input()
+    elif choice == "11":
+        company.set_fine_input()
+    elif choice == "12":
+        company.set_check_input()
+    elif choice == "13":
+        company.set_check_freq_input()
+    elif choice == "14":
+        company.set_contact_person_input()
+
+    print("Company updated")
+    company.print_data()
+    pause_terminal()
+
+
+def delete_company():
+    choice = select_company()
+
+    should_delete = input("\nAre you sure you want to delete this company? (y/N): ")
+    if should_delete.lower() == "y":
+        del G.companies[int(choice)]
+        print("Company deleted")
+        pause_terminal()
+        return
+    else:
+        print("Company not deleted")
+        pause_terminal()
+        return
+
+
+def select_visit_report() -> int:
+    clear_terminal()
+
+    company_list = {}
+
+    for i, report in enumerate(G.reports):
+        date_formatted = datetime.strptime(report.get_visit_date(), "%Y%m%d")
+        print_str = f"{i + 1}. Company code: {report.get_company_code()}, Inspector code: {report.get_inspector_code()}, Visit date: {date_formatted}"
+        if report.get_company_code() not in company_list:
+            company_list[report.get_company_code()] = []
+
+        company_list[report.get_company_code()].append(print_str)
+
+    for k, v in company_list.items():
+        print("")
+        for company in v:
+            print(company)
+        print("")
+
+    choice = input("Enter choice: ")
+    if choice.isdigit() and (0 > int(choice) and int(choice) <= len(G.reports)):
+        print("Invalid choice")
+        pause_terminal()
+        return select_visit_report()
+
+    clear_terminal()
+    return int(choice) - 1
+
+
+def add_visit_report():
+    selected = select_visit_report()
+
+
+def edit_visit_report():
+    selected = select_visit_report()
+
+
+def delete_visit_report():
+    choice = select_visit_report()
+
+    should_delete = input("\nAre you sure you want to delete this report? (y/N): ")
+    if should_delete.lower() == "y":
+        del G.reports[int(choice)]
+        print("Report deleted")
+        pause_terminal()
+        return
+    else:
+        print("Report not deleted")
+        pause_terminal()
+        return
+
+
 def main():
-    report_filtered_menu = menu_controller.Menu("Report filtered data", [
-        ("Print reports per inspector", print_reports_per_inspector),
-        ("Print reports per company", print_reports_per_company)
+    report_filtered_menu = menu_controller.Menu("Visit report filtered data", [
+        ("Print visit reports per inspector", print_reports_per_inspector),
+        ("Print visit reports per company", print_reports_per_company)
     ])
     display_data_menu = menu_controller.Menu("Display data", [
         ("Print inspector data", lambda: [print_data(G.inspectors)]),
         ("Print company data", lambda: [print_data(G.companies)]),
-        ("Report filtered data", report_filtered_menu)
+        ("Visit report filtered data", report_filtered_menu)
     ])
-
 
     plot_measurement_data_menu = menu_controller.Menu("Plot measurement data", [
         ("co2", lambda: [plot_gas(0)]),
@@ -209,10 +377,28 @@ def main():
         ("Calculate fines", show_company_fines)
     ], lambda: [load_measurement_file() if G.loaded_measurement is None else None])
 
+    select_company_edit_menu = menu_controller.Menu("Edit company data", [
+        ("Add company", add_company),
+        ("Edit company", edit_company),
+        ("Delete company", delete_company)
+    ])
+
+    select_visit_reports_menu = menu_controller.Menu("Select visit report", [
+        ("Add visit report", add_visit_report),
+        ("Edit visit report", edit_visit_report),
+        ("Delete visit report", delete_visit_report)
+    ])
+
+    manage_data_menu = menu_controller.Menu("Manage data", [
+        ("Manage company data", select_company_edit_menu),
+        ("Manage visit report", select_visit_reports_menu)
+    ])
 
     main_menu = menu_controller.Menu("Main Menu", [
         ("Display data", display_data_menu),
-        ("Measurement file", measurement_file_menu)
+        ("Measurement file", measurement_file_menu),
+        ("Manage data", manage_data_menu),
+        ("Exit and save", lambda: exit(0))
     ])
 
     main_menu.display()
